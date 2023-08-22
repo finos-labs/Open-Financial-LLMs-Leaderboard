6e039c4e38aa4fdc36fe70163adc70ac31600939
Clémentine <cle.fourrier@gmail.com>
2023-08-22 15:35:38 +0200

    Flagged model per discussion

--- src/auto_leaderboard/model_metadata_flags.py
+++ src/auto_leaderboard/model_metadata_flags.py
@@ -1,5 +1,6 @@
 # Model name to forum discussion id
 FLAGGED_MODELS = {
     "Voicelab/trurl-2-13b": "https://huggingface.co/spaces/HuggingFaceH4/open_llm_leaderboard/discussions/202",
-    "deepnight-research/llama-2-70B-inst": "https://huggingface.co/spaces/HuggingFaceH4/open_llm_leaderboard/discussions/207"
+    "deepnight-research/llama-2-70B-inst": "https://huggingface.co/spaces/HuggingFaceH4/open_llm_leaderboard/discussions/207",
+    "Aspik101/trurl-2-13b-pl-instruct_unload": "https://huggingface.co/spaces/HuggingFaceH4/open_llm_leaderboard/discussions/213",
 }
