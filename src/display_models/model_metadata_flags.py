bbd101d1b3827ee8d03c46086e864d658917afcc
Clémentine Fourrier <clefourrier@users.noreply.huggingface.co>
2023-08-29 06:59:09 +0000

    Flagging Fredithefish/ReasonixPajama-3B-HF

--- src/display_models/model_metadata_flags.py
+++ src/display_models/model_metadata_flags.py
@@ -4,6 +4,7 @@ FLAGGED_MODELS = {
     "Voicelab/trurl-2-13b": "https://huggingface.co/spaces/HuggingFaceH4/open_llm_leaderboard/discussions/202",
     "deepnight-research/llama-2-70B-inst": "https://huggingface.co/spaces/HuggingFaceH4/open_llm_leaderboard/discussions/207",
     "Aspik101/trurl-2-13b-pl-instruct_unload": "https://huggingface.co/spaces/HuggingFaceH4/open_llm_leaderboard/discussions/213",
+    "Fredithefish/ReasonixPajama-3B-HF": "https://huggingface.co/spaces/HuggingFaceH4/open_llm_leaderboard/discussions/236",
 }
 
 # Models which have been requested by orgs to not be submitted on the leaderboard
