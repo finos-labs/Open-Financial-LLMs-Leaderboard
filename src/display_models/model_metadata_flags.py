77c51de9965828a597357217379db56f5b67395c
Clémentine Fourrier <clefourrier@users.noreply.huggingface.co>
2023-09-06 16:34:03 +0000

    flagged gaodrew/gaodrew-gorgonzola-13b

--- src/display_models/model_metadata_flags.py
+++ src/display_models/model_metadata_flags.py
@@ -6,6 +6,7 @@ FLAGGED_MODELS = {
     "Aspik101/trurl-2-13b-pl-instruct_unload": "https://huggingface.co/spaces/HuggingFaceH4/open_llm_leaderboard/discussions/213",
     "Fredithefish/ReasonixPajama-3B-HF": "https://huggingface.co/spaces/HuggingFaceH4/open_llm_leaderboard/discussions/236",
     "TigerResearch/tigerbot-7b-sft-v1": "https://huggingface.co/spaces/HuggingFaceH4/open_llm_leaderboard/discussions/237",
+    "gaodrew/gaodrew-gorgonzola-13b": "https://huggingface.co/spaces/HuggingFaceH4/open_llm_leaderboard/discussions/215",
 }
 
 # Models which have been requested by orgs to not be submitted on the leaderboard
