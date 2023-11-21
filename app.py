d084b260ca1c8f233d17f6367cd1e438f0a0316e
Clémentine <cle.fourrier@gmail.com>
2023-11-21 15:56:23 +0100

    adding pull back

--- app.py
+++ app.py
@@ -39,25 +39,25 @@ from src.tools.plots import (
 
 def restart_space():
     API.restart_space(repo_id=REPO_ID, token=H4_TOKEN)
-if False:
-    try:
-        print(EVAL_REQUESTS_PATH)
-        snapshot_download(
-            repo_id=QUEUE_REPO, local_dir=EVAL_REQUESTS_PATH, repo_type="dataset", tqdm_class=None, etag_timeout=30
-        )
-    except Exception:
-        restart_space()
-    try:
-        print(EVAL_RESULTS_PATH)
-        snapshot_download(
-            repo_id=RESULTS_REPO, local_dir=EVAL_RESULTS_PATH, repo_type="dataset", tqdm_class=None, etag_timeout=30
-        )
-    except Exception:
-        restart_space()
+
+try:
+    print(EVAL_REQUESTS_PATH)
+    snapshot_download(
+        repo_id=QUEUE_REPO, local_dir=EVAL_REQUESTS_PATH, repo_type="dataset", tqdm_class=None, etag_timeout=30
+    )
+except Exception:
+    restart_space()
+try:
+    print(EVAL_RESULTS_PATH)
+    snapshot_download(
+        repo_id=RESULTS_REPO, local_dir=EVAL_RESULTS_PATH, repo_type="dataset", tqdm_class=None, etag_timeout=30
+    )
+except Exception:
+    restart_space()
 
 
 raw_data, original_df = get_leaderboard_df(EVAL_RESULTS_PATH, EVAL_REQUESTS_PATH, COLS, BENCHMARK_COLS)
-# update_collections(original_df.copy())
+update_collections(original_df.copy())
 leaderboard_df = original_df.copy()
 
 plot_df = create_plot_df(create_scores_df(raw_data))
